9ba575445d78e82092dade8c3f123f29066ea65a
pushupmouse <vutranhungduc@gmail.com>
2022-08-15 15:06:50 +0700

    fixtures

---
+++ migrations/Version20220815075253.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220815075253 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE brand (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, date_founded DATE NOT NULL, image VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE category (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE laptop (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, date DATE NOT NULL, price DOUBLE PRECISION NOT NULL, quantity INT NOT NULL, color VARCHAR(255) NOT NULL, image VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE manufacturer (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, image VARCHAR(255) NOT NULL, address VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE brand');
+        $this->addSql('DROP TABLE category');
+        $this->addSql('DROP TABLE laptop');
+        $this->addSql('DROP TABLE manufacturer');
+    }
+}
---
+++ src/DataFixtures/LaptopFixtures.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Laptop;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+
+class LaptopFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        for ($i=0; $i < 20; $i++) { 
+            $laptop = new Laptop;
+            $laptop->setName("Laptop $i");
+            $laptop->setDate(\DateTime::createFromFormat('Y/m/d','2022/01/13'));
+            $laptop->setPrice((float)(rand(300,2000)));
+            $laptop->setQuantity(rand(10,100));
+            $laptop->setColor("Black");
+            $laptop->setImage("https://maytinhanphat.vn/img/image/tin/283/sinh-vien-nen-chon-laptop-van-phong-moi-hay-cu-3.jpg");
+            $manager->persist($laptop);
+        }
+        $manager->flush();
+    }
+}
