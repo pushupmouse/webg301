29cdf21bddf4446791798e6624c87fad0965ff21
VanhNguyen03 <anhnvgch210312@fpt.edu.vn>
2022-08-15 16:44:30 +0700

    origin

--- src/DataFixtures/OriginFixtures.php
+++ src/DataFixtures/OriginFixtures.php
@@ -9,8 +9,15 @@ class OriginFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        //$country = ["America", "France", "China", "Germany", "Japan"];
+
+        for ($i=0; $i < 10; $i++) {
+            //$key = array_rand($titles,1)
+            $brand = new Brand;
+            //$brand->setTitle($titles[$key]);
+            $brand->setName("Brand $i")
+                  ->setImage("https://preview.redd.it/1jnsfe05vjg21.jpg?auto=webp&s=aaaa7fb926c3ea6c7f92b278d1050d206d78c0bb"); 
+        }
 
         $manager->flush();
     }
