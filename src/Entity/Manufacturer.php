4b6472e4a2284a23a31452530e3a5b0dcdc3971c
pushupmouse <vutranhungduc@gmail.com>
2022-08-16 11:06:33 +0700

    relation & form

--- src/Entity/Manufacturer.php
+++ src/Entity/Manufacturer.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\ManufacturerRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: ManufacturerRepository::class)]
@@ -21,6 +23,14 @@ class Manufacturer
 
     #[ORM\Column(type: 'string', length: 255)]
     private $address;
+
+    #[ORM\ManyToMany(targetEntity: Laptop::class, mappedBy: 'manufacturers')]
+    private $laptops;
+
+    public function __construct()
+    {
+        $this->laptops = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -59,6 +69,33 @@ class Manufacturer
     public function setAddress(string $address): self
     {
         $this->address = $address;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Laptop>
+     */
+    public function getLaptops(): Collection
+    {
+        return $this->laptops;
+    }
+
+    public function addLaptop(Laptop $laptop): self
+    {
+        if (!$this->laptops->contains($laptop)) {
+            $this->laptops[] = $laptop;
+            $laptop->addManufacturer($this);
+        }
+
+        return $this;
+    }
+
+    public function removeLaptop(Laptop $laptop): self
+    {
+        if ($this->laptops->removeElement($laptop)) {
+            $laptop->removeManufacturer($this);
+        }
 
         return $this;
     }
