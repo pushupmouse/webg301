4d897c23227d4c09f479104602d3af7809d51210
pushupmouse <vutranhungduc@gmail.com>
2022-08-16 11:16:31 +0700

    zzz

--- src/Form/CategoryType.php
+++ src/Form/CategoryType.php
@@ -7,7 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class CategoryFormType extends AbstractType
+class CategoryType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
