b574fc5c4bb62e2305ba796269b15a7f52ab6606
VanhNguyen03 <anhnvgch210312@fpt.edu.vn>
2022-08-16 17:39:25 +0700

    css home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,11 +8,9 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
-    public function index(): Response
+    #[Route('/home', name: 'home')]
+    public function index()
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        return $this->render('home/homepage.html.twig');
     }
 }
