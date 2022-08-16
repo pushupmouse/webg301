757092c664c61ab61560ea9a87e3ba2cc4c06cc7
pushupmouse <vutranhungduc@gmail.com>
2022-08-16 10:43:16 +0700

    controller template done

--- src/Controller/LaptopController.php
+++ src/Controller/LaptopController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Entity\Laptop;
 use App\Repository\LaptopRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,6 +13,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 #[Route('/laptop')]
 class LaptopController extends AbstractController
 {
+    #[Route('/shop', name: 'laptop_shop')]
+    public function laptopShop()
+    {
+        $laptops = $this->getDoctrine()->getRepository(Laptop::class)->findAll();
+        return $this->render('laptop/shop.html.twig',
+        [
+            'laptops' => $laptops
+        ]);
+    }
+
     #[Route('/index', name: 'laptop_index')]
     public function laptopIndex()
     {
@@ -20,4 +32,38 @@ class LaptopController extends AbstractController
             'laptops' => $laptops
         ]);
     }
+
+    #[Route('/detail/{id}', name: 'laptop_detail')]
+    public function laptopDetail($id)
+    {
+        $laptop = $this->getDoctrine()->getRepository(Laptop::class)->find($id);
+        if ($laptop == null) {
+            $this->addFlash('Warning', 'Invalid laptop id !');
+            return $this->redirectToRoute('laptop_index');
+        }
+        return $this->render('laptop/detail.html.twig',
+        [
+            'laptop' => $laptop
+        ]);
+    }
+
+    #[Route('/delete/{id}', name: 'laptop_delete')]
+    public function laptopDelete ($id, ManagerRegistry $managerRegistry)
+    {
+        
+    }
+
+    #[Route('/add', name: 'laptop_add')]
+    public function laptopAdd (Request $request)
+    {
+        
+    }
+
+    #[Route('/edit/{id}', name: 'laptop_edit')]
+    public function laptopEdit ($id, Request $request)
+    {
+
+    }
 }
+
+    
